cce837c3929566e2240f6606d6d5f3760f2318f6
bfsgr <bfsgr.code@protonmail.com>
2024-01-28 21:40:12 -0300

    add migrations, models and controllers

---
+++ database/migrations/2024_01_28_043408_create_customer.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id();
+            $table->string('cpf');
+            $table->date('birthdate');
+            $table->string('cep');
+            $table->string('street');
+            $table->string('number');
+            $table->string('district');
+            $table->string('city');
+            $table->string('state');
+            $table->string('address_info')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customer');
+    }
+};
---
+++ database/migrations/2024_01_28_183007_create_job.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('bath');
+            $table->boolean('groom');
+            $table->datetime('date');
+            $table->foreignId('pet_id')->constrained('pet');
+            $table->foreignId('worker_id')->constrained('worker');
+            $table->dateTime('accepted_at')->nullable();
+            $table->dateTime('rejected_at')->nullable();
+            $table->dateTime('preparing_at')->nullable();
+            $table->dateTime('bath_started_at')->nullable();
+            $table->dateTime('groom_started_at')->nullable();
+            $table->dateTime('finished_at')->nullable();
+            $table->dateTime('notified_at')->nullable();
+            $table->dateTime('delivered_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job');
+    }
+};
